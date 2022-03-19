cd95e80dbb3105993b969762bc88df38c9924c3b
Bowo <wibowosulistio@gmail.com>
2022-03-19 14:54:51 +0700

    v1.5.1 release

--- system-dashboard.php
+++ system-dashboard.php
@@ -16,7 +16,7 @@
  * Plugin Name:       System Dashboard
  * Plugin URI:        https://wordpress.org/plugins/system-dashboard/
  * Description:       Centralized dashboard to monitor various WordPress components, stats and data, including the server.
- * Version:           1.5.0
+ * Version:           1.5.1
  * Author:            Bowo
  * Author URI:        https://bowo.io
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SYSTEM_DASHBOARD_VERSION', '1.5.0' );
+define( 'SYSTEM_DASHBOARD_VERSION', '1.5.1' );
 
 /**
  * The code that runs during plugin activation.
