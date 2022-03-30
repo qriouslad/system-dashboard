0ee3ba93a012516e2fac4baa763231ed5009d131
Bowo <wibowosulistio@gmail.com>
2022-03-30 13:45:14 +0700

    Dequeue public styles and scripts

--- includes/class-system-dashboard.php
+++ includes/class-system-dashboard.php
@@ -244,8 +244,8 @@ class System_Dashboard {
 
 		$plugin_public = new System_Dashboard_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
